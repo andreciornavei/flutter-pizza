9cc3bccd18db32e8b2b09d00b5eefea1d9b7475d
Andre Ciornavei <andre.ciornavei@gmail.com>
2020-07-27 23:24:04 -0300

    move example project to external project

--- example/lib/controller.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_pizza_example/models/pizza_cart.model.dart';
-import 'package:get/state_manager.dart';
-
-class AppController extends GetxController{
-  RxList<PizzaCart> cart = RxList<PizzaCart>([]);
-}
--- example/lib/utils/json.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:convert';
-
-import 'package:flutter/services.dart';
-
-loadJson(String asset) async {
-  String data = await rootBundle.loadString(asset);
-  return json.decode(data);
-}
